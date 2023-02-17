ac8119a67fd544c26eedc48e8d93af4a94771505
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-02-17 17:25:32 -0600

    create screens

---
+++ src/screens/NoteList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NoteList = () => {
+  return (
+    <View>
+      <Text>NoteList</Text>
+    </View>
+  )
+}
+
+export default NoteList
+
+const styles = StyleSheet.create({})
