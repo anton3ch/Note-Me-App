de1196bea8c5637d1c4c72851fdf1bc6e1cb4fa4
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-02-17 18:33:09 -0600

    edit app.js

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import Header from './src/components/Header';
+import { LinearGradient } from 'expo-linear-gradient';
+import { ScreenType } from './src/constants/constants';
+import AddNote from './src/screens/AddNote';
+import HomeScreen from './src/screens/HomeScreen';
+import NoteList from './src/screens/NoteList';
+
 
 export default function App() {
+  const [screen, setScreen] = useState(ScreenType.home);
+  let content;
+  if (screen === ScreenType.addNote) {
+    content = <AddNote/>
+  } else if (screen === ScreenType.noteList) {
+    content = <NoteList/>
+  } else if (screen === ScreenType.home) {
+    content = <HomeScreen/>
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+        // Background Linear Gradient
+        colors={['white', 'rgba(60,60,60, 0.1)']}
+        style={styles.gradient}>
+        <Header style={styles.header}
+        onScreenChange={(screen) => {setScreen(screen)}}
+        />
+      <SafeAreaView style={styles.container}>
+        
+        {content}
+        <StatusBar style="auto" />
+        
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(171, 171, 171, 0.1)',
+    padding: 30,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  gradient: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  text: {
+
+    width: "100%",
+    textAlign: "center",
+
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
 });
---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const ScreenType = {
+  home: "HOME",
+  addNote: "ADD_NOTE",
+  noteList: "NOTE_LIST"}
