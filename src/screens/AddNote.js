b6fcff9d0a5df0c3050a90e0542ef8034babdb78
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-02-24 11:28:18 -0600

    create add note screen

--- src/screens/AddNote.js
+++ src/screens/AddNote.js
@@ -1,14 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native'
+import React, { useState, Component } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ScreenType } from '../constants/constants';
+import { LinearGradient } from 'expo-linear-gradient';
+import { v4 } from 'uuid';
+import {Dimensions} from 'react-native';
+import { VibrancyView } from "@react-native-community/blur/";
+import { BlurView } from 'expo-blur';
 
-const AddNote = () => {
+
+const AddNote = (props) => {
+  const [note, setNote] = useState();
+
+  const saveNote = async () => {
+    const value = await AsyncStorage.getItem("NOTES")
+    const n = value ? JSON.parse(value) : []
+    n.push({id: v4(), note: note, key: v4()})
+    await AsyncStorage.setItem("NOTES", JSON.stringify(n))
+    //.then(() => props.onScreenChange(ScreenType.noteList))
+    setNote("")
+  }
+  
   return (
-    <View>
-      <Text>AddNote</Text>
-    </View>
+    <LinearGradient
+        // Background Linear Gradient
+        colors={['white', 'rgba(60,60,60, 0.1)']}
+        style={styles.gradient}>
+
+    <BlurView intensity={40} tint="light" style={styles.blurContainer}>
+    <ScrollView style={styles.container} keyboardDismissMode='interactive'>
+      <TextInput
+      value={note}
+      onChangeText={setNote}
+      multiline={true}
+      autoFocus
+      selectionColor='#fff'
+      style={styles.input}
+      />
+      <KeyboardAvoidingView keyboardVerticalOffset={150} behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.bottom}> 
+        <Button
+        style={styles.button}
+        appearance='filled' onPress={saveNote}
+        title="Save"
+        > SAVE </Button>
+      </KeyboardAvoidingView>
+    </ScrollView>
+      </BlurView>
+    
+    </LinearGradient>
   )
 }
 
 export default AddNote
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:
+  {
+    flex: 1,
+    padding: 30,
+    paddingTop: 80,
+    // backgroundColor: 'rgba(171, 171, 171, 0.1)'
+  },
+  blurContainer: {
+    flex: 1,
+  },
+  bottom:
+  {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 36,
+  },
+  button:
+  {
+    marginBottom: 30,
+  },
+  gradient: {
+    flex: 1,
+  },
+  input: {
+    width: Dimensions.get('window').width,
+    height: 250,
+  },
+})
